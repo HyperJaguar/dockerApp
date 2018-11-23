418dc806387923f47906f88442fe6399c38739ef
abhayan <sabhayans@gmail.com>
2018-11-23 10:00:00 +0530

    detailed docker compoer

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,6 +11,17 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+
+var connection = mysql.createConnection({
+    host     : 'mysql',
+    user     : 'root',
+    password : 'root'
+});
+
+
+// connection.query('USE my_database');
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -19,7 +31,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
+app.get('/', function(req, res){
+    connection.query('SELECT User FROM mysql.user;', function(err, rows){
+        // console.log(rows);
+        console.log(err);
+        // res.render('index', {users : rows});
+    });
+});
+
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
